54b0b00489e05b7a29ebc7b619b2533b1d1d9805
fan19900404 <125920969@qq.com>
2017-12-02 18:56:36 +0800

    修改输出为ES5

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const querystring_1 = require("querystring");
+exports.__esModule = true;
+var querystring_1 = require("querystring");
 /**
  * 浏览器标准刷新方法
  * @param num 值为0，为了兼容vue-router的API
  */
-const go = (num) => {
+var go = function (num) {
     if (num === 0) {
         location.reload(true);
     }
@@ -15,25 +15,26 @@ const go = (num) => {
  * @param {any} router vue-router实例
  * @export
  */
-function reload(router = { go }) {
+function reload(router) {
+    if (router === void 0) { router = { go: go }; }
     if (/(iPhone|iPad|iPod)/i.test(navigator.userAgent)) {
-        const hash = location.hash;
-        const origin = location.origin;
-        const pathname = location.pathname;
-        let search = location.search;
+        var hash = location.hash;
+        var origin = location.origin;
+        var pathname = location.pathname;
+        var search = location.search;
         if (search) {
-            const searchObj = querystring_1.parse(search.slice(1, -1));
+            var searchObj = querystring_1.parse(search.slice(1, -1));
             searchObj.jxytime = String(Date.now());
-            search = `?${querystring_1.stringify(searchObj)}`;
+            search = "?" + querystring_1.stringify(searchObj);
         }
         else {
-            search = `?jxytime=${10000 * Math.random()}`;
+            search = "?jxytime=" + 10000 * Math.random();
         }
-        location.replace(`${origin}${pathname}${search}${hash}`);
+        location.replace("" + origin + pathname + search + hash);
     }
     else {
         router.go(0);
     }
 }
-exports.default = reload;
+exports["default"] = reload;
 //# sourceMappingURL=index.js.map
