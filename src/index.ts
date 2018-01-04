b8e3bff27f095ee731c62b3060da91316c93efc0
fan19900404 <125920969@qq.com>
2018-01-04 20:08:19 +0800

    将部分方法提到外面

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,48 @@ import { parse, stringify } from "querystring";
  * 浏览器标准刷新方法
  * @param num 值为0，为了兼容vue-router的API
  */
-const go = (num: number) => {
+const go = (num: number): void => {
   if (num === 0) {
     location.reload(true);
   }
 };
 
 /**
- * 防止缓存，强制刷新
- * @param {any} router vue-router实例
- * @export
+ * 判断是否在IOS中
  */
-export default function reload(router = { go }): any {
-  if (/(iPhone|iPad|iPod)/i.test(navigator.userAgent)) {
+const isIOS = (): boolean => /(iPhone|iPad|iPod)/i.test(navigator.userAgent);
+
+/**
+ * 获取时间戳
+ */
+const getNowTimeStamp = (): string => `${Date.now()}`;
+
+/**
+ * 处理url，获得更新时间戳之后的url
+ */
+const getUrl = (): string => {
+  let { origin, pathname, search, hash } = location;
+  if (search) {
+    const searchObj = parse(search.slice(1, -1));
+    searchObj.jxytime = getNowTimeStamp();
+    search = `?${stringify(searchObj)}`;
+  } else {
+    search = `?jxytime=${getNowTimeStamp()}`;
+  }
+  return `${origin}${pathname}${search}${hash}`;
+};
+
+/**
+ * 防止缓存，强制刷新
+ * @param router vue-router实例
+ */
+function reload(router = { go }): void {
+  if (isIOS()) {
     // 为了兼容IOS
-    let { origin, pathname, search, hash } = location;
-    if (search) {
-      const searchObj = parse(search.slice(1, -1));
-      searchObj.jxytime = String(Date.now());
-      search = `?${stringify(searchObj)}`;
-    } else {
-      search = `?jxytime=${10000 * Math.random()}`;
-    }
-    location.replace(`${origin}${pathname}${search}${hash}`);
+    location.replace(getUrl());
   } else {
     router.go(0);
   }
 }
+
+export default reload;
